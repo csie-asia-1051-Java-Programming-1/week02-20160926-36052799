eb779f8185c069591a7ba889dbb80fbab1e83c76
36052799 <105021035@live.asia.edu.tw>
2016-09-26 16:07:42 +0800

    3

--- src/ex/ex02_105021035.java
+++ src/ex/ex02_105021035.java
@@ -1,17 +1,26 @@
 package ex;
+import java.util.*;
 /*
  * Topic: 有一圓形，直徑為200，且中心座標為(0,0)。
  * 	請寫一支程式可以輸入「點」的座標，並判斷「點」是否在圓形的範圍內。
  * 	如果「點」的位置剛好在邊界的話也算是在圓形範圍內(例：x=100，y=0)。
  * Date: 2016/09/26
- * Author: 1050210XX 周永振老師
+ * Author: 105021035 王文宏
  */
 
-public class ex02_1050210XX {
+public class ex02_105021035 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+	    Scanner sca=new Scanner(System.in);
+	    int x=sca.nextInt();
+	    int y=sca.nextInt();
+	    double z=Math.sqrt((x-0)^2+(y-0)^2);
+	    System.out.print(z);
+	    if(z>100){System.out.print("點在園外");
+	  
+	    }
+	    else System.out.print("點在園內");
 	}
-
+	
 }
--- src/ex/ex03_105021035.java
+++ src/ex/ex03_105021035.java
@@ -1,17 +1,31 @@
 package ex;
+import java.util.*;
 /*
  * Topic: 假設某個停車場的費率是停車2小時以內，每半小時30元，超過2小時，但未滿4小時的部份，
  * 	每半小時40元，超過4小時以上的部份，每半小時60元，未滿半小時部分不計費。
  * 	如果您從早上10點23分停到下午3點20分，請撰寫程式計算共需繳交的停車費。
  * Date: 2016/09/26
- * Author: 1050210XX 周永振老師
+ * Author: 105021035 王文宏
  */
 
-public class ex03_1050210XX {
+public class ex03_105021035 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        Scanner sca=new Scanner(System.in);
+        int a=sca.nextInt();
+        int b=sca.nextInt();
+        int c=sca.nextInt();
+        int d=sca.nextInt();
+        int e=(c*60+d)-(a*60-b)/30;
+        int o=0;
+        if(e<=4){o=e*30;
+        
+        }
+        else if(8<e && e<4){o=120+(e-4)*40;
 	}
-
+        else{o=120+160+(e-8*60);
+        }
+        System.out.print(o);
+	}
 }
