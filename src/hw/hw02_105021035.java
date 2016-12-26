42df31af7e9630692ffebac9b8b7efcb2001f560
36052799 <105021035@live.asia.edu.tw>
2016-12-26 15:14:31 +0800

    Update and rename hw02_1050210XX.java to hw02_105021035.java

---
+++ src/hw/hw02_105021035.java
@@ -0,0 +1,19 @@
+package hw;
+import java.util.Scanner;
+public class hw02_1050210XX {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		System.out.println("輸入1為男性 2為女性");
+		int a = scn.nextInt();
+		System.out.println("身高:");
+		int b = scn.nextInt();
+		
+		if (s==1){
+			System.out.print((b-80)*0.7);}
+		else if (s==1){
+			System.out.print((h-80)*0.7);}
+        	else if(s==2){
+        		System.out.print((h-70)*0.6);}}}
+
