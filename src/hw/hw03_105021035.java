2a7213676e79668ff87868781124c3bd22982e3f
36052799 <105021035@live.asia.edu.tw>
2016-12-26 15:16:47 +0800

    Update and rename hw03_1050210XX.java to hw03_105021035.java

---
+++ src/hw/hw03_105021035.java
@@ -0,0 +1,16 @@
+package hw;
+
+import java.util.Scanner;
+public class hw03_1050210XX {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner scn = new Scanner(System.in);
+		int a = scn.nextInt();
+		if(a%400==0){
+        System.out.print("潤年");}
+    else if(a%100==0){
+	    System.out.print("平年");}
+	else if(a%4==0){
+		System.out.print("潤年");}}}
+
